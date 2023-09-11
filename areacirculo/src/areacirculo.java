3987e9d224270e830591f72e62532ceb44155a7f
Lizbeth Miramontes <141800965+arylizz@users.noreply.github.com>
2023-09-10 22:23:38 -0600

    areacirculo 1.0
    Primera version de la app de Java que nos permite calcular el área de un círculo.

---
+++ areacirculo/src/areacirculo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+/**
+ * Esta clase nos ayuda a calcular el área de un círculo.
+ * @author Lizbeth Monserrat Miramontes Flores
+ */
+
+public class areacirculo {
+    public static void main(String[] args) {
+        double radio;
+        double area;
+
+        System.out.println("¡Hola, vamos a calcular el área de un círculo!");
+
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("Ingrese el tamaño del radio");
+            radio = entrada.nextDouble();
+        }
+
+        area = Math.PI * Math.pow(radio, 2);
+
+        System.out.println("El área del círculo es:" + area);
+    }
+}
